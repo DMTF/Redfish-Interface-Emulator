f38607ea91e48053d084d982775a4f82ff681810
Richard Hughes <richard@hughsie.com>
2017-09-04 16:46:03 +0100

    Add shebangs and change the tools to have execute permissons
    This allows a Linux use to just type ./emulator.py and for the emulator to
    start up without worrying about the python path.

--- codegen/dynamic_codegen.py
+++ codegen/dynamic_codegen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+#
 # Copyright Notice:
 # Copyright 2016 Distributed Management Task Force, Inc. All rights reserved.
 # License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Interface-Emulator/LICENSE.md
